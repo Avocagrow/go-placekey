2a5bb1625d6098a97224351a2af4a966ba62ce83
Steven Turturo <steven@getadmiral.com>
2022-04-28 17:34:05 -0700

    chore: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/theoriginalstove/go-placekey
+
+go 1.18
---
+++ placekey.go
@@ -0,0 +1,13 @@
+// Package goplacekey provices bindings for interacting with the Placekey REST APIs.
+package goplacekey
+
+// Public constants
+const (
+	PlacekeyURL                    = "https://api.placekey.io"
+	DefaultMaxNetworkRetries int64 = 2
+)
+
+// Key is the Placekey API from the Placekey developer 
+var Key string
+
+
